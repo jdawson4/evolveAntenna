f584dc5646482c0e056db45e9100299da6097da7
jdawson4 <jacob.dawson@me.com>
2022-10-30 18:35:23 -0400

    mostly comments

--- fitness.py
+++ fitness.py
@@ -30,14 +30,24 @@ def frequency_response():
         necpp.nec_delete(nec)
 
 def fitness(wiresInput=[(1, 1, 1), (2, 2, 2)]):
+    # I'll make a few notes for frequencies I might wanna receive here:
+    # AM radio broadcasts are 540 kHz up to 1700 kHz (1.7 MHz). These are vertically polarized
+    # FM radio broadcasts are from 88 MHz to 108 MHz, and should be easy to receive. Probably vertically polarized
+    # NOAA (& other) weather satellites transmit 137-138 MHz signals from all directions above the horizon with right-handed polarization.
+    # TV transmits linearly polarized (horizontally?) at a few weird frequencies, always from a single source (directionality matters). I'll try and list them here (all US):
+    #   VHF low is between 54 and 88 MHz
+    #   VHF high is between 174 and 216 MHz
+    #   UHF is between 470 and 700 Mhz (seems a little unclear what the upper bound is?)
+    # (higher gain required for higher frequencies because of atmospheric attenuation!)
+
     # sorta want these as parameters but whatever
-    frequency=137
+    frequency=54
     #polarizationType=''
-    polarizationType='RHP'
+    polarizationType='LIN'
     epsilon = 0.00001 # this parameter makes sure that we're not inserting wires that are too similar!
 
-    # ok so we're having an issue where the input isn't taking a list of tuples, it's just processing as a... list.
-    # Let's do this:
+    # we have to do some weird preprocessing because the library we're
+    # using is based on compiled c/fortran code so it's finicky.
     if len(wiresInput)<3:
         print(999)
         return 999
@@ -49,20 +59,13 @@ def fitness(wiresInput=[(1, 1, 1), (2, 2, 2)]):
         wiresTemp = []
         for i in wiresInput:
             j+=1
-            wiresTemp.append(i)
             if ((j%3)==0):
+                wiresTemp.append(abs(i)) # ensure that coord 3 is always above 0!
                 wires.append(tuple(wiresTemp))
                 wiresTemp = []
-
-    '''newWires = []
-    for coords in wires:
-        newCoords = []
-        for coord in coords:
-            newCoord = round(coord, 2) # round to nearest cm
-            newCoords.append(newCoord)
-        newWires.append(tuple(newCoords))
-    wires = newWires'''
-    allCoords = []
+            else:
+                wiresTemp.append(i)
+    '''allCoords = []
     for coords in wires:
         for coord in coords:
             if coord == 0.0:
@@ -74,7 +77,7 @@ def fitness(wiresInput=[(1, 1, 1), (2, 2, 2)]):
                         print(999)
                         #print('here!')
                         return 999
-            allCoords.append(coord)
+            allCoords.append(coord)'''
     print(wires)
 
     try:
@@ -86,25 +89,49 @@ def fitness(wiresInput=[(1, 1, 1), (2, 2, 2)]):
                 print(999)
                 return 999 # can't go below ground!
             x1, y1, z1 = previousEnd
-            handle_nec(necpp.nec_wire(context, i, 15, x1, y1, z1, x2, y2, z2, 0.005, 1, 1))
+            handle_nec(necpp.nec_wire(context, i, 15, x1, y1, z1, x2, y2, z2, 0.001, 1, 1))
             previousEnd = (x2, y2, z2)
             i+=1
         handle_nec(necpp.nec_geometry_complete(context, 1)) # says that we've now set the antenna geometry
-        handle_nec(necpp.nec_gn_card(context, 1, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)) # quadruped ground plane where each wire is 5 mm in radius and .5 meters long
-        handle_nec(necpp.nec_fr_card(context, 0, 5, frequency, 0.25)) # checks the base frequency, as well as 4 frequencies 0.25 mhz higher
+        handle_nec(necpp.nec_gn_card(context, 1, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)) # defines ground plane as an infinite surface. Much simpler that way.
+        
+        # here we define the frequencies we're looking for. The parameters here
+        # define a frequency, and then x other frequencies y steps above that
+        # base frequency. Important! This defines how broad of a band our\
+        # antenna works for!
+        #handle_nec(necpp.nec_fr_card(context, 0, 5, frequency, 0.25)) # checks the base frequency, as well as 4 frequencies 0.25 mhz higher
+        handle_nec(necpp.nec_fr_card(context, 0, 100, frequency, 6)) # checks 100 frequencies above the given freq, each 6 MHz apart. For tv, input 54 as base frequency.
+
+        
+        # Polarization determines A LOT about antenna architecture. We have
+        # options for prcessing left-handed, right-handed, and linear
+        # polarization.
+        # Left- and right-handed are used by satellites, whereas most
+        # terrestrial applications are linearly polarized. Natural radio
+        # sources tend to be unpolarized, but there are exceptions!
         if polarizationType=='RHP':
             polarization=2
+        elif polarizationType=='LHP':
+            polarization=3
+        elif polarizationType=='':
+            polarization=1
+        elif polarizationType=='LIN':
+            polarization=1
         else:
             polarization=1
-        handle_nec(necpp.nec_ex_card(context, polarization, 36, 36, 0, 0, 0, 0, 10, 10, 0)) # I have no idea what this does. Something to do with excitation? Polarization?
+
+
+        handle_nec(necpp.nec_ex_card(context, polarization, 36, 36, 0, 0, 0, 0, 10, 10, 0)) # here's where polarization gets applied
         handle_nec(necpp.nec_rp_card(context, 0, 90, 1, 0,5,0,0, 0, 90, 1, 0, 0, 0)) # this is... quite important. It describes the radiation patterns--gain and so on. I can't quite work it out, but this is crucial.
         
-        # from the example, so maybe ignore:
-        #z = complex(necpp.nec_impedance_real(context,result_index), necpp.nec_impedance_imag(context,result_index))
-        #print("f=%0.2fMHz \t(%6.1f,%+6.1fI) Ohms" % (frequency, z.real, z.imag))
         
         # here's the important part:
-        gain = necpp.nec_gain_mean(context, 1)
+        #gain = necpp.nec_gain_mean(context, 1)
+        #gain = necpp.nec_gain_max(context, 1)
+        gain = necpp.nec_gain_min(context, 1)
+        # MEAN gain in order to make... something
+        # MAX gain to make a directional antenna,
+        # MIN gain to make an omnidirectional? Not too sure!
 
         necpp.nec_delete(context) # delete now that we have our calculations
     except:
@@ -116,10 +143,12 @@ def fitness(wiresInput=[(1, 1, 1), (2, 2, 2)]):
     print(gain)
 
     return -gain
-    # I am slightly lying--we return *average* gain at one particular frequency
-    # any angle...negated, so that our model can *minimize* this number.
+    # I am slightly lying--we return gain... negated, so that our model
+    # can *minimize* the negative gain (that is, maximize the gain.)
+    # This weird inversion is happening because most optimization algorithms
+    # are actually seeking to minimize mathematical functions.
 
 if __name__=='__main__':
     #gain = fitness(frequency=137, polarizationType='RHP', wires=[(0.5,0.5,0.5),(1,1,1)])
-    gain = fitness(wiresInput=[0.0,0.0,0.52])
+    gain = fitness(wiresInput=[0.0,0.3,1.3])
     print(gain)
--- optimize.py
+++ optimize.py
@@ -12,34 +12,40 @@
 # the evolutionary approach!
 
 from fitness import *
-from scipy.optimize import minimize
+from scipy.optimize import minimize, basinhopping
 
 def optimize():
     # we'll compare our results against a monopole that I hand-design
     # for the purpose of receiving 137 MHz
     baseAntenna = [0.0,0.0,0.52] # z is up!
-    # a 52 cm monopole receives 137 MHz, but not well with
-    # right-hand-polarized signal, so that's why we need this
-    # optimization algorithm!
+    # a 52 cm monopole receives 137 MHz
     #baseAntennaGain = fitness(frequency=137,polarizationType='RHP',wires=baseAntenna)
     baseAntennaGain = fitness(wiresInput=baseAntenna)
     print(f"Hand-designed antenna has a gain of {-baseAntennaGain}")
 
     # okay... now how does scipy's optimizer work?
     # res = minimize(fun = simple_poly, x0 = 20)
-    # ^ is an example of working optimize() code
+    # ^ is an example of working optimize() code, where fun() is just
+    # a minimizable lambda function (a parabola or something)
 
     # soooo....
-    results = minimize(
+    '''results = minimize(
         fun = fitness,
         #x0 = [0.0,0.0,0.13, 0.0,0.0,0.26, 0.0,0.0,0.39, 0.0,0.0,0.52],
         #x0 = [0.0,0.0,0.26, 0.0,0.0,0.52],
         #x0 = [0.0,0.0,0.1733, 0.0,0.0,0.3467, 0.0,0.0,0.52],
-        x0 = [0.1,0.2,0.3, 0.4,0.5,0.6, 0.7,0.8,0.9],
+        #x0 = [0.01,0.02,0.03, 0.04,0.05,0.06, 0.07,0.08,0.09, 0.15,0.2,0.3, 1,2,3],
+        x0 = [0.01,0.02,0.03, 0.04,0.05,0.06, 0.07,0.08,0.09, 0.15,0.2,0.3],
         options = {
             "disp": True,
             "maxiter": 10
         }
+    )'''
+    results = basinhopping(
+        func = fitness,
+        x0 = [0.0,0.0,0.1, 0.0,0.0,0.35, 0.0,0.0,0.7],
+        interval=10,
+        niter=10,
     )
     print(results)
     return results.x
